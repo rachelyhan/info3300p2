b6d8812a6cc47734e6e2dcc4365ed44b76d0ecf0
Crystal Wu <cw683@cornell.edu>
2021-05-01 15:13:08 -0400

    us hover

--- usMap.js
+++ usMap.js
@@ -1,4 +1,4 @@
-// PART 2: US MAP WITH COUNTIES WITH UNEMPLOYMENT RATE (Rachel and Crystal)
+// DRAWS US MAP IN VISUALIZATION 2, AND CREATES HOVER TOOLTIP FOR IT
 
 const colorsArray2 = ["lightgrey", "cyan"];
 const colorScale2 = d3.scaleQuantize()
@@ -7,10 +7,7 @@ const colorScale2 = d3.scaleQuantize()
 
 var filters = {};
 
-function getFilters() {
-  return filters
-};
-
+//returns true if county fulfills all filter criteria
 function matchCounties(county, data) {
   var pass = true
   Object.values(filters).forEach(filterFunc => {
@@ -19,6 +16,7 @@ function matchCounties(county, data) {
   return pass
 };
 
+//updates colors for counties
 function updateUS(counties, us_path, data) {
   us_svg.selectAll("path.counties").data(counties.features)
     .join("path")
@@ -27,17 +25,38 @@ function updateUS(counties, us_path, data) {
     .attr("fill", d => colorScale2(Number(matchCounties(d.id, data))));
 }
 
-function drawUS(counties, us_path, statesMesh, data) {
-  us_svg.selectAll("path.counties").data(counties.features)
+//draws US map
+function drawUS(counties, us_path, statesMesh, data, county_id, us) {
+  const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+  // const mapWidth = us_svg.attr("width") - margin.left - margin.right;
+  // const mapHeight = us_svg.attr("height") - margin.top - margin.bottom;
+  const map = us_svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  map.selectAll("path.counties").data(counties.features)
     .join("path")
     .attr("class", "counties")
     .attr("d", us_path)
     .attr("fill", d => colorScale2(Number(matchCounties(d.id, data))));
-  us_svg.append("path").datum(statesMesh)
+  map.append("path").datum(statesMesh)
     .attr("class", "outline")
     .attr("d", us_path);
 
-  // usTooltip(data)
+  let momesh = map.append("path")
+    .attr("class", "select_outline")
+    .attr("d", "");
+
+  map.append("path")
+    .attr("class", "mouseover_outline")
+    .attr("d", "")
+    .attr("id", "momesh")
+
+  var state_projection = d3.geoAlbersUsa().scale(1).translate([0, 0]);
+  var state_path = d3.geoPath().projection(state_projection);
+
+  var mo = topojson.mesh(us, us.objects.counties, function (a, b) { return a.id === county_id.toString() || b.id === county_id.toString(); });
+  momesh.datum(mo).attr("d", state_path)
+  console.log(mo)
 }
 
 function makeSlider(container, label, attribute, sliderHeight, sliderWidth, counties, us_path, statesMesh, selected_county, data) {
@@ -143,8 +162,95 @@ function makeSlider(container, label, attribute, sliderHeight, sliderWidth, coun
   }
   canvas.append("g").attr("class", "brush").call(brush)
   // .call(brush.move, defaultSelection);
+}
 
-  // Object.values(filters).forEach(filterFunc => {
-  //   // console.log(filters[attribute])
-  // });
+function usTooltip(data, us) {
+  const tooltipWidth = 300;
+  const tooltipHeight = 135;
+  let svg = d3.select("#us_map");
+
+  let tooltip = svg.append("g")
+    .attr("class", "us_tooltip")
+    .style("visibility", "hidden")
+    .attr("id", "tooltip")
+
+  tooltip.append("rect")
+    .attr("fill", "black")
+    .attr("opacity", 0.9)
+    .attr("x", -tooltipWidth / 2.0)
+    .attr("y", 0)
+    .attr("width", tooltipWidth)
+    .attr("height", tooltipHeight)
+
+  //text
+  const attributes = ["name", "employment", "income", "literacy", "college", "highschool"]
+  var y = 10
+
+  attributes.forEach((d) => {
+    tooltip.append("text")
+      .attr("fill", "white")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "hanging")
+      .attr("x", 0)
+      .attr("y", y)
+      .attr("id", d)
+
+    y += 20
+  })
+
+  d3.selectAll(".counties").on("mouseenter", mouseEntersPlot);
+  d3.selectAll(".counties").on("mouseout", mouseLeavesPlot);
+
+
+
+  function mouseEntersPlot() {
+    const svg = d3.select(this.parentNode.parentNode)
+    tooltip = svg.select("#tooltip")
+    let county = d3.select(this).datum();
+    let countyID = county.id;
+
+    //calculating translation and scale for tooltip placement
+    var states = topojson.feature(us, us.objects.states)
+    selected_state = states.features.filter(function (d) {
+      return d.id === countyID.substring(0, 2);
+    });
+
+    var state_projection = d3.geoAlbersUsa().scale(1).translate([0, 0]);
+    var state_path = d3.geoPath().projection(state_projection);
+
+    bounds = state_path.bounds(selected_state[0]);
+    var state_scale = 1300
+    var state_translate = [487.5 + (bounds[0][0]) / 2, 400 + (bounds[0][1]) / 2];
+
+    state_projection.scale(state_scale)
+      .translate(state_translate);
+    state_projection.translate(state_translate);
+
+    bounds = state_path.bounds(county);
+    let xPos = (bounds[0][0] + bounds[1][0]) / 2.0;
+    let yPos = bounds[1][1];
+
+    //highlight outline
+    var mo = topojson.mesh(us, us.objects.counties, function (a, b) { return a.id === countyID || b.id === countyID });
+    svg.select("#momesh").datum(mo).attr("d", state_path)
+
+    //update and show tooltip
+    tooltip.style("visibility", "visible")
+    tooltip.attr("transform", `translate(${xPos},${yPos})`);
+
+    svg.select("#name").text(data[Number(countyID)]["county_name"] + ", " + data[Number(countyID)]["state"])
+    svg.select("#employment").text((data[Number(countyID)]["employment_rate"] * 100).toString() + "% employed");
+    svg.select("#income").text("Median Household Income" + data[Number(countyID)]["income"]);
+    svg.select("#literacy").text((data[Number(countyID)]["literacy"] * 100).toString() + "% at Level 3 literacy");
+    svg.select("#college").text(data[Number(countyID)]["college"] + "% have bachelor's degrees");
+    svg.select("#highschool").text(data[Number(countyID)]["high_school"] + "% graduated high school");
+  }
+
+
+
+  function mouseLeavesPlot() {
+    const svg = d3.select(this.parentNode.parentNode)
+    tooltip = svg.select("#tooltip").style("visibility", "hidden");
+    svg.select("#momesh").attr("d", "");
+  }
 }
